0c9daa8449a874bcb250dc562e7fc6e287130c09
wa-hii <wahyuyuliarto@mail.ugm.ac.id>
2026-09-07 19:03:52 +0700

    ganti parameter valve dan ubah fase

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@
 // ─────────────────────────────────────────────────────
 #define ACQ_COLLECTION_SECONDS 120 // durasi menghirup aroma kopi (detik)
 #define ACQ_PURGE_SECONDS 120      // durasi purging ke udara bebas (detik)
-#define ACQ_REPETITIONS 5          // jumlah pengulangan siklus
+#define ACQ_REPETITIONS 50         // jumlah pengulangan siklus
 
 // ─── Feature Flags
 // ────────────────────────────────────────────────────────────
